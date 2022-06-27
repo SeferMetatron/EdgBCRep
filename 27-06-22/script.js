b8b0a2e256a71991ed05aa651d713bb45c6963c1
SeferMetatron <mistificatorlol@gmail.com>
2022-06-27 17:11:18 +0200

    just do it

--- 27-06-22/script.js
+++ 27-06-22/script.js
@@ -4,28 +4,28 @@ const newEl = (el) => document.createElement(el);
 const navEl = newEl("nav");
 const divEl = newEl("div");
 const userFirst = prompt("Enter your first name");
-const userSecond = prompt("Enter your second name");
+// const userSecond = prompt("Enter your second name");
 // const arrayN = [{ name: userFirst, surname: userSecond }];
 navEl.className = "nav";
 body.append(navEl);
 navEl.append(divEl);
 
-localStorage.setItem("username", userFirst);
-localStorage.setItem("surname", userSecond);
-localStorage.getItem("username");
-localStorage.getItem("surname");
-navEl.innerHTML = userFirst + ` ` + userSecond;
+// localStorage.setItem("username", userFirst);
+// localStorage.setItem("surname", userSecond);
+// localStorage.getItem("username");
+// localStorage.getItem("surname");
+navEl.innerHTML = userFirst;
 
 try {
-  if (userFirst || userSecond !== null) {
-  }
-  throw new Error("Check the data entered");
+  if (localStorage.getItem("username") !== userFirst) {
+    localStorage.setItem("username", userFirst);
+  } else throw Error("Enter your credentials");
 } catch (error) {
-  console.warn(error);
+  console.log(error);
   document.body.append(
-    (document.createElement("p").textContent = "404, please try again")
+    (document.createElement("p").textContent = "404, try again")
   );
-  throw new Error("I am firing up my laser");
+  throw Error("Uh Oh, something broke");
 } finally {
   console.log("All your base are belong to us");
 }
